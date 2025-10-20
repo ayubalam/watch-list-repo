20fc8e0e59b5e691fd33e223e03ba479a5307c69
Ayub Alam <ayubalam.2024cse@sece.ac.in>
2025-10-20 18:03:43 +0530

    Initial project setup via Spring Initializr

---
+++ src/test/java/com/example/stockwatchlist/StockWatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockwatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockWatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
